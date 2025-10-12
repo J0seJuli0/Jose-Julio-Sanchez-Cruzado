56ed2b2c75e14118a9803638e14cff514417f965
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2025-10-12 10:37:18 -0500

    update about me deseing

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-
-}
+export class AboutComponent { }
